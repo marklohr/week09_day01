72aaea36edefbe7a57eb18fa507856d2708992a1
Mark Lohr <markuslohr@gmail.com>
2015-03-02 16:37:40 -0500

    functioning calculator, needs more styling

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,27 +1,60 @@
-var indexCtrl = calculator.controller('indexCtrl', function($scope) {
-    $scope.some_text = {quote: "'eeeeeeyyyyyy!", quoteMaster: "The Fonz"};
-    $scope.quotes = [ 
-      {quote: "I'll be back!", quoteMaster: "Arnold Schwarzzzz"},
-      {quote: "It's time to kick ass and chew bubble gum, and I'm all outta gum", quoteMaster: 'Duke'},
-      {quote: "One appletini, light on the tini", quoteMaster: "JD"},
-      {quote: "Respect, Ryan", quoteMaster: "Adam"}
-    ];
-    $scope.formQuote = '';
-    $scope.formQuoteMaster = '';
+var indexCtrl = calculator.controller('indexCtrl', function($scope){
+  $scope.calcInput = '';
+  $scope.userInputs = [];
 
-    $scope.changeQuote = function() {      
-      $scope.some_text = _.sample($scope.quotes);
-    };
+  $scope.clearCalculation = function() {
+    $scope.calcInput = '';
+    $scope.userInputs = [];
+  };
 
-    $scope.submitForm = function() {
-      $scope.quotes.push(
-        { quote: $scope.formQuote, quoteMaster: $scope.formQuoteMaster }
-      );
-      $scope.formQuote = '';
-      $scope.formQuoteMaster = '';
-    };
+  $scope.userNumber1 = function() {
+    $scope.calcInput = $scope.calcInput.concat(1);
+  };
+  $scope.userNumber2 = function() {
+    $scope.calcInput = $scope.calcInput.concat(2);
+  };
+  $scope.userNumber3 = function() {
+    $scope.calcInput = $scope.calcInput.concat(3);
+  };
+  $scope.userNumber4 = function() {
+    $scope.calcInput = $scope.calcInput.concat(4);
+  };
+  $scope.userNumber5 = function() {
+    $scope.calcInput = $scope.calcInput.concat(5);
+  };
+  $scope.userNumber6 = function() {
+    $scope.calcInput = $scope.calcInput.concat(6);
+  };
+  $scope.userNumber7 = function() {
+    $scope.calcInput = $scope.calcInput.concat(7);
+  };
+  $scope.userNumber8 = function() {
+    $scope.calcInput = $scope.calcInput.concat(8);
+  };
+  $scope.userNumber9 = function() {
+    $scope.calcInput = $scope.calcInput.concat(9);
+  };
+  $scope.userNumber0 = function() {
+    $scope.calcInput = $scope.calcInput.concat(0);
+  };
+  $scope.userNumberPlus = function() {
+    $scope.userInputs.push($scope.calcInput);
+    console.log($scope.userInputs);
+    $scope.calcInput = '';
+    console.log($scope.calcInput);
+  };
+  $scope.userNumberMinus = function() {
+    $scope.userInputs.push($scope.calcInput);
+    $scope.calcInput = '-';
+    console.log($scope.userInputs);
+  };
+  $scope.userNumberEquals = function() {
+    // add final item to array and then reduce
+    $scope.userInputs.push($scope.calcInput);
+    $scope.userInputs = $scope.userInputs.map(Number);
+    var total = _.reduce($scope.userInputs, function(memo, num){return memo + num; },0);
+    $scope.calcInput = total;
+    $scope.userInputs = [];
+  };
+
 });
-
-// var indexCtrl = addressBook.controller('someOtherCtrl', function($scope) {
-//     $scope.some_text = "Welcome to Earth. - Will Smith";
-// });
