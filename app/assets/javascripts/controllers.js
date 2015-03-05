e16bb8872fd131f96f7e2dda00ad3d255c21b33d
Mark Lohr <markuslohr@gmail.com>
2015-03-05 15:32:35 -0500

    refactoring code

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -7,36 +7,41 @@ var indexCtrl = calculator.controller('indexCtrl', function($scope){
     $scope.userInputs = [];
   };
 
-  $scope.userNumber1 = function() {
-    $scope.calcInput = $scope.calcInput.concat(1);
+  $scope.userNumber = function(calcNumber) {
+    // console.log($target);
+    $scope.calcInput = $scope.calcInput.concat(calcNumber);
   };
-  $scope.userNumber2 = function() {
-    $scope.calcInput = $scope.calcInput.concat(2);
-  };
-  $scope.userNumber3 = function() {
-    $scope.calcInput = $scope.calcInput.concat(3);
-  };
-  $scope.userNumber4 = function() {
-    $scope.calcInput = $scope.calcInput.concat(4);
-  };
-  $scope.userNumber5 = function() {
-    $scope.calcInput = $scope.calcInput.concat(5);
-  };
-  $scope.userNumber6 = function() {
-    $scope.calcInput = $scope.calcInput.concat(6);
-  };
-  $scope.userNumber7 = function() {
-    $scope.calcInput = $scope.calcInput.concat(7);
-  };
-  $scope.userNumber8 = function() {
-    $scope.calcInput = $scope.calcInput.concat(8);
-  };
-  $scope.userNumber9 = function() {
-    $scope.calcInput = $scope.calcInput.concat(9);
-  };
-  $scope.userNumber0 = function() {
-    $scope.calcInput = $scope.calcInput.concat(0);
-  };
+
+  // $scope.userNumber1 = function() {
+  //   $scope.calcInput = $scope.calcInput.concat(1);
+  // };
+  // $scope.userNumber2 = function() {
+  //   $scope.calcInput = $scope.calcInput.concat(2);
+  // };
+  // $scope.userNumber3 = function() {
+  //   $scope.calcInput = $scope.calcInput.concat(3);
+  // };
+  // $scope.userNumber4 = function() {
+  //   $scope.calcInput = $scope.calcInput.concat(4);
+  // };
+  // $scope.userNumber5 = function() {
+  //   $scope.calcInput = $scope.calcInput.concat(5);
+  // };
+  // $scope.userNumber6 = function() {
+  //   $scope.calcInput = $scope.calcInput.concat(6);
+  // };
+  // $scope.userNumber7 = function() {
+  //   $scope.calcInput = $scope.calcInput.concat(7);
+  // };
+  // $scope.userNumber8 = function() {
+  //   $scope.calcInput = $scope.calcInput.concat(8);
+  // };
+  // $scope.userNumber9 = function() {
+  //   $scope.calcInput = $scope.calcInput.concat(9);
+  // };
+  // $scope.userNumber0 = function() {
+  //   $scope.calcInput = $scope.calcInput.concat(0);
+  // };
   $scope.userNumberPlus = function() {
     $scope.userInputs.push($scope.calcInput);
     console.log($scope.userInputs);
